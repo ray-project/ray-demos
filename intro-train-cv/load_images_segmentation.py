9f7a77a416fe1265e1598943d6258d4d9dd51086
Adam Breindel <c-adam.breindel@anyscale.com>
2023-05-24 10:08:38 -0700

    add Ray Train w CV example notebooks and supporting scripts

---
+++ intro-train-cv/load_images_segmentation.py
@@ -0,0 +1,10 @@
+import tasks_helper_utils as t_utils
+
+SMALL_DATA = False
+DATASET_NAME = "scene_parse_150"
+
+# Load data from the Hugging Face datasets repository.
+if SMALL_DATA:
+    train_dataset = load_dataset(DATASET_NAME, split="train[:160]")
+else:
+    train_dataset = load_dataset(DATASET_NAME, split="train")
